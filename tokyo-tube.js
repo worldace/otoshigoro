539105932e57929acbe18e45547e0643d9b96d51
worldace <tokaiknight@yahoo.co.jp>
2016-02-17 20:17:35 +0900

    no message

--- tokyo-tube.js
+++ tokyo-tube.js
@@ -9,9 +9,9 @@ var apiurl = player_config + "?vkey=" + ID[1];
 var xhr = new XMLHttpRequest();
 xhr.open("GET", apiurl);
 xhr.addEventListener("load", function(){
-    var xml = xhr.responseXML;
-    var video_url = xml.getElementsByTagName("video")[0].getElementsByTagName("src")[0].firstChild.nodeValue;
-    //<xml><video><src>動画URL</src></video></xml> DOM操作はワケワカラン
+    var video_url = xhr.responseXML.querySelector("video src").textContent;
+    //<xml><video><src>動画URL</src></video></xml> ↓でも可。DOM操作はワケワカラン
+    //var video_url = xhr.responseXML.getElementsByTagName("video")[0].getElementsByTagName("src")[0].firstChild.nodeValue;
 
     if(video_url){
         オトシゴロ.ダウンロード(video_url);
